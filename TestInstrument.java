a1d73884fb1a2ace61a1922da29494af1c506885
dyl-alex <dalexander_02@arcadia.edu>
2021-09-21 11:56:42 -0400

    filled out stuff that was emailed

--- TestInstrument.java
+++ TestInstrument.java
@@ -11,26 +11,42 @@ public class TestInstrument implements Instrument {
     TestInstrument() {
         strings = new ArrayList<>();
         for (int i = 0; i < 37; i++) {
-            strings.add(new LinkedList<>());
+            double freq = 440.0 * Math.pow(2.0, (i - 24.0 / 12.0));
+            int size = (int)(44100.0 / freq);
+            LinkedList<Double> ll = new LinkedList<>();
+            for (int j = 0; j < size; j++) {
+                ll.add(0.0);
+            }
+        strings.add(ll);
         }
     }
 
     @Override
     public void pluck(char key){
-        double freq = 440.0 * Math.pow(2.0, (key % 37 - 24) / 12)
+        double freq = 440.0 * Math.pow(2.0, (key % 37 - 24) / 12);
         int size = (int)(44100.0 / freq);
         for (int gs = 0; gs < 37; gs++) {
-
+            for (int f = 0; f < size; f++) {
+                double r = 0.4;
+                strings.get(gs).set(f, r);
+            }
         }
     }
 
+    @Override
     public void tick() {
-
+        for (int gs = 0; gs < 37; gs++) {
+            LinkedList<Double> buffer = strings.get(gs);
+            double magic = 0.994 * (buffer.get(0) + buffer.get(1)) / 2.0;
+            buffer.add(magic);
+            buffer.remove();
+        }
     }
 
+    @Override
     public double superposition() {
+        // TODO Auto-generated method stub
         return 0;
-        
     }
 
 }
