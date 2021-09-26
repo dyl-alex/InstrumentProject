0dc097f2b49dfb07cc0757f49249f9debe682ced
dyl-alex <dalexander_02@arcadia.edu>
2021-09-26 16:47:11 -0400

    small changes

--- TestInstrument.java
+++ TestInstrument.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 // ArrayList -> guitar strings
 // LinkedList -> the frequency buffer
-// double -> a random number in [-0.5, 0.5]
+// Double -> a random number in [-0.5, 0.5]
 
 public class TestInstrument implements Instrument {
 
@@ -11,37 +11,37 @@ public class TestInstrument implements Instrument {
     TestInstrument() {
         strings = new ArrayList<>();
         for (int i = 0; i < 37; i++) {
-            double freq = 440.0 * Math.pow(2.0, (i - 24.0 / 12.0));
-            int size = (int)(44100.0 / freq);
+            double freq = 440.0 * Math.pow(2.0, (i - 24) / 12.0);
+            int size = (int)(44100 / freq);
             LinkedList<Double> ll = new LinkedList<>();
             for (int j = 0; j < size; j++) {
                 ll.add(0.0);
             }
-        strings.add(ll);
+            strings.add(ll);
         }
     }
 
     @Override
-    public void pluck(char key){
-        double freq = 440.0 * Math.pow(2.0, (key % 37 - 24) / 12);
+    public void pluck(char key) {
+        double freq = 440.0 * Math.pow(2.0, (key % 37 - 24) / 12.0);
         int size = (int)(44100.0 / freq);
         for (int gs = 0; gs < 37; gs++) {
             for (int f = 0; f < size; f++) {
-                double r = 0.4;
+                double r = 0.4; // random [-0.5; 0.5]
                 strings.get(gs).set(f, r);
             }
         }
     }
 
     @Override
-    public void tick() {
-        for (int gs = 0; gs < 37; gs++) {
-            LinkedList<Double> buffer = strings.get(gs);
-            double magic = 0.994 * (buffer.get(0) + buffer.get(1)) / 2.0;
-            buffer.add(magic);
-            buffer.remove();
+        public void tick() {
+            for (int gs = 0; gs < 37; gs++) {
+                LinkedList<Double> buffer = strings.get(gs);
+                double magic = 0.994 * (buffer.get(0) + buffer.get(1)) / 2.0;
+                buffer.add(magic);
+                buffer.remove();
+            }
         }
-    }
 
     @Override
     public double superposition() {
